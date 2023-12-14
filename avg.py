81c1bc93bd9069c5b03f68d1d247df50e6d05f01
MEHUL <shahmehul702@gmail.com>
2023-12-14 20:32:50 +0530

    first commit

---
+++ avg.py
@@ -0,0 +1,14 @@
+def calculate_average(numbers):
+    total = sum(numbers)
+    average = total / len(numbers)
+    return average
+
+num_count = int(input("Enter the number of values: "))
+user_numbers = []
+
+for i in range(num_count):
+    user_input = float(input(f"Enter number {i + 1}: "))
+    user_numbers.append(user_input)
+
+avg_result = calculate_average(user_numbers)
+print("Average:", avg_result)
